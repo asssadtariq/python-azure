ab021552c93353924572468e284d4f8b326acc4c
Asad Tariq <asadtariq.at66@gmail.com>
2025-03-11 19:32:31 +0500

    chore: new function / route added for testing purpose

--- function_app.py
+++ function_app.py
@@ -7,6 +7,24 @@ from app.app import execute_logic
 app = func.FunctionApp()
 
 
+@app.route(route="TestFunction", auth_level=func.AuthLevel.ANONYMOUS)
+def TestFunction(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    params = req.params
+
+    if params['mode'] == str(1):
+        return func.HttpResponse(
+            "pass",
+            status_code=200,
+        )
+
+    return func.HttpResponse(
+        "fail",
+        status_code=200,
+    )
+
+
 @app.route(route="BlobFunction", auth_level=func.AuthLevel.ANONYMOUS)
 def BlobFunction(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
@@ -14,6 +32,6 @@ def BlobFunction(req: func.HttpRequest) -> func.HttpResponse:
     execute_logic()
 
     return func.HttpResponse(
-        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        "pass",
         status_code=200,
     )
